8fb0e6c044ac8b1d84e8ae72187ac69b08f03cd5
lastnameswayne <tiarnan1109@gmail.com>
2021-02-13 22:06:43 +0100

    deploy heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const flash = require("connect-flash");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(flash());
